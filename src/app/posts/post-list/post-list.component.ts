f9a134b6d19d09520f4043b53a23a9c659c1cafa
Freddy Merlin <freddymerlin258@gmail.com>
2021-09-21 11:17:10 -0400

    Display Outline Completed

--- src/app/posts/post-list/post-list.component.ts
+++ src/app/posts/post-list/post-list.component.ts
@@ -15,15 +15,65 @@ import { PostsService } from "../posts.service";
 export class PostListComponent implements OnInit, OnDestroy {
   posts:Post[] = [];
   isLoading = false;
+
   postsPerPage = 2;
   userAuth = false;
   currentPage = 1;
   userId: string;
+  add = false;
   pageSizeOptions = [1,2,5,10];
   public totalPosts = 0;
   private postsSub!: Subscription;
   private authStatusSub: Subscription;
   constructor(public postsService: PostsService, private authService: AuthService){}
+  removeAdd(){
+    this.add = false;
+  }
+  copy(text){
+    console.log(text)
+    navigator.clipboard.writeText(text);
+    alert("Copied the contact: " + text);
+  }
+  addMore(){
+
+    let activate = (!this.add)
+
+
+    this.add = !this.add;
+    let postActive = document.getElementsByClassName("post-info").length;
+    for(let i = 0; i < postActive; i++){
+      if (activate){
+        let addButton = document.getElementsByClassName("fill")[i] as HTMLElement;
+        addButton.classList.add("active")
+      }
+      else{
+        let addButton = document.getElementsByClassName("fill")[i] as HTMLElement;
+        addButton.classList.remove("active")
+      }
+      let element = document.getElementsByClassName("post-info")[i] as HTMLElement;
+      if (element.classList.contains('height')){
+          element.classList.remove("height");
+      }
+      else{
+        element.classList.add("height");
+      }
+    }
+  }
+  open(){
+    this.add = false;
+    let postActive = document.getElementsByClassName("post-info").length;
+    for(let i = 0; i < postActive; i++){
+      let addButton = document.getElementsByClassName("fill")[i] as HTMLElement;
+      addButton.classList.remove("active")
+      let element = document.getElementsByClassName("post-info")[i] as HTMLElement;
+      if (element.classList.contains('height')){
+        element.classList.remove("height");
+      }
+    }
+  }
+
+
+
   ngOnInit()
   {
     this.isLoading = true;
