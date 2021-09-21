e73b943c138b70702997fa4bc32eb728701dd746
Freddy Merlin <freddymerlin258@gmail.com>
2021-09-21 11:42:22 -0400

    Changed apiUrl

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,4 +1,4 @@
 export const environment = {
   production: true,
-  apiUrl: "https://warm-hollows-24652.herokuapp.com"
+  apiUrl: "https://spartan-store.herokuapp.com"
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,7 +4,7 @@
 
 export const environment = {
   production: false,
-  apiUrl: "https://warm-hollows-24652.herokuapp.com"
+  apiUrl: "https://spartan-store.herokuapp.com"
 };
 
 /*
